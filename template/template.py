8a044649c6bddc7b6643f6c2da55dd018e1466a3
NidalZabade <nidal1zabade@gmail.com>
2023-06-27 13:12:31 +0300

    project structure

---
+++ template/template.py
@@ -0,0 +1,104 @@
+import numpy as np
+import csv
+import sys
+
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+
+TEST_SIZE = 0.3
+K = 3
+
+
+class NN:
+    def __init__(self, trainingFeatures, trainingLabels) -> None:
+        self.trainingFeatures = trainingFeatures
+        self.trainingLabels = trainingLabels
+
+    def predict(self, features, k):
+        """
+        Given a list of features vectors of testing examples
+        return the predicted class labels (list of either 0s or 1s)
+        using the k nearest neighbors
+        """
+        raise NotImplementedError
+
+
+def load_data(filename):
+    """
+    Load spam data from a CSV file `filename` and convert into a list of
+    features vectors and a list of target labels. Return a tuple (features, labels).
+
+    features vectors should be a list of lists, where each list contains the
+    57 features vectors
+
+    labels should be the corresponding list of labels, where each label
+    is 1 if spam, and 0 otherwise.
+    """
+    raise NotImplementedError
+
+
+def preprocess(features):
+    """
+    normalize each feature by subtracting the mean value in each
+    feature and dividing by the standard deviation
+    """
+    raise NotImplementedError
+
+
+def train_mlp_model(features, labels):
+    """
+    Given a list of features lists and a list of labels, return a
+    fitted MLP model trained on the data using sklearn implementation.
+    """
+    raise NotImplementedError
+
+
+def evaluate(labels, predictions):
+    """
+    Given a list of actual labels and a list of predicted labels,
+    return (accuracy, precision, recall, f1).
+
+    Assume each label is either a 1 (positive) or 0 (negative).
+    """
+    raise NotImplementedError
+
+
+def main():
+    # Check command-line arguments
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python template.py ./spambase.csv")
+
+    # Load data from spreadsheet and split into train and test sets
+    features, labels = load_data(sys.argv[1])
+    features = preprocess(features)
+    X_train, X_test, y_train, y_test = train_test_split(
+        features, labels, test_size=TEST_SIZE
+    )
+
+    # Train a k-NN model and make predictions
+    model_nn = NN(X_train, y_train)
+    predictions = model_nn.predict(X_test, K)
+    accuracy, precision, recall, f1 = evaluate(y_test, predictions)
+
+    # Print results
+    print("**** 1-Nearest Neighbor Results ****")
+    print("Accuracy: ", accuracy)
+    print("Precision: ", precision)
+    print("Recall: ", recall)
+    print("F1: ", f1)
+
+    # Train an MLP model and make predictions
+    model = train_mlp_model(X_train, y_train)
+    predictions = model.predict(X_test)
+    accuracy, precision, recall, f1 = evaluate(y_test, predictions)
+
+    # Print results
+    print("**** MLP Results ****")
+    print("Accuracy: ", accuracy)
+    print("Precision: ", precision)
+    print("Recall: ", recall)
+    print("F1: ", f1)
+
+
+if __name__ == "__main__":
+    main()
