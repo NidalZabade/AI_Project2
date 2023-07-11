1f83536dc248642ffa3cb518d27210681b8c4a00
NidalZabade <nidal1zabade@gmail.com>
2023-07-11 18:30:43 +0300

    done

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import time
