7134463caf67663569721f3a798f04e160557caf
NidalZabade <nidal1zabade@gmail.com>
2023-07-11 18:27:53 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# AI_Project2
+# AI_Project2 - Train and test different ML models (KNN, MLP)
+
+## Project Description
+
+This is the second project for the course Artificial Intelligence (ENCS3340). The goal of this project is to train and test different machine learning models (KNN, MLP). The project written in Python
+
+- [Project Description](./assets/AI_project2.pdf)
+
+## Project Structure
+
+```bash
+AI_Project2
+├── assets
+│   └── AI_project2.pdf
+├── data
+│   └── spambase.csv
+├── docs
+│   └── AI_Report.pdf
+├── src
+│   ├── main.py
+│   └── spambase.csv
+├── template
+│   └── template.py
+├── .gitignore
+├── README.md
+└── requirements.txt
+```
+
+## Installation and Running
+
+To install the project, run the following command in the terminal:
+
+```bash
+git clone https://github.com/NidalZabade/AI_Project2.git
+cd AI_Project2
+```
+
+To install the required packages, run the following command in the terminal:
+
+```bash
+pip install -r requirements.txt
+```
+
+To run the project, run the following command in the terminal:
+
+```bash
+python src/main.py
+```
+
+## Project Report
+
+## Contributors
+
+- [Nidal Zabade](https://github.com/NidalZabade)
+- [Alaa Saleem](https://github.com/alaasaleem)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 import numpy as np
-import csv
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import time
+
 
 TEST_SIZE = 0.3
 K = 3
@@ -13,38 +17,29 @@ class NN:
         self.trainingFeatures = trainingFeatures
         self.trainingLabels = trainingLabels
 
-    def distance(self, x, y):
-        """
-        Given two vectors x and y, return the Euclidean distance between them
-        """
-        return np.linalg.norm(x - y)
-
-    def getMajority(self, neighbors):
-        """
-        Given a list of neighbors, return the majority label
-        """
-        labels = [neighbor[0] for neighbor in neighbors]
-        return max(set(labels), key=labels.count)
-
     def predict(self, features, k):
         """
-        Given a list of features vectors of testing examples
+        Given a list of features vectors of testing examples,
         return the predicted class labels (list of either 0s or 1s)
-        using the k nearest neighbors
+        using the k nearest neighbors.
         """
         predictions = []
         for feature in features:
             distances = []
             for i in range(len(self.trainingFeatures)):
+                # calculate distance between feature and trainingFeatures using np.linalg.norm (euclidean distance = sqrt(sum((x - y)^2)))
                 distances.append(
                     (
                         self.trainingLabels[i],
-                        self.distance(feature, self.trainingFeatures[i]),
+                        np.linalg.norm(
+                            feature - self.trainingFeatures[i]
+                        ),  # euclidean distance = sqrt(sum((x - y)^2))
                     )
                 )
-            distances.sort(key=lambda x: x[1])
-            neighbors = distances[:k]
-            predictions.append(self.getMajority(neighbors))
+            distances.sort(key=lambda x: x[1])  # sort by distance
+            neighbors = distances[:k]  # get k nearest neighbors
+            labels = [neighbor[0] for neighbor in neighbors]  # get labels of neighbors
+            predictions.append(max(set(labels), key=labels.count))  # get majority label
         return predictions
 
 
@@ -63,6 +58,7 @@ def load_data(filename):
     df = pd.read_csv(filename, header=None)  # read csv file
     features = df.iloc[:, :-1].values  # get features
     labels = df.iloc[:, -1].values  # get labels
+    print("Data loaded.")
     return features, labels
 
 
@@ -71,12 +67,9 @@ def preprocess(features):
     normalize each feature by subtracting the mean value in each
     feature and dividing by the standard deviation
     """
-
-    for i in range(len(features[0])):
-        mean = np.mean(features[:, i])
-        std = np.std(features[:, i])
-        features[:, i] = (features[:, i] - mean) / std
-    return features
+    return (features - np.mean(features, axis=0)) / np.std(
+        features, axis=0
+    )  # normalize (x - mean) / std
 
 
 def train_mlp_model(features, labels):
@@ -84,8 +77,21 @@ def train_mlp_model(features, labels):
     Given a list of features lists and a list of labels, return a
     fitted MLP model trained on the data using sklearn implementation.
     """
-    model = MLPClassifier(random_state=1, max_iter=300).fit(features, labels)
+    model = MLPClassifier(hidden_layer_sizes=(100, 100), max_iter=1000).fit(
+        features, labels
+    )  # train MLP model
     return model
+
+
+# confusion matrix
+def confusion_matrix(labels, predictions):
+    tp = np.sum(np.logical_and(labels, predictions))  # true positive
+    fp = np.sum(np.logical_and(np.logical_not(labels), predictions))  # false positive
+    tn = np.sum(
+        np.logical_and(np.logical_not(labels), np.logical_not(predictions))
+    )  # true negative
+    fn = np.sum(np.logical_and(labels, np.logical_not(predictions)))  # false negative
+    return tp, fp, tn, fn
 
 
 def evaluate(labels, predictions):
@@ -95,20 +101,7 @@ def evaluate(labels, predictions):
 
     Assume each label is either a 1 (positive) or 0 (negative).
     """
-    tp = 0
-    fp = 0
-    tn = 0
-    fn = 0
-    for i in range(len(labels)):
-        if labels[i] == 1 and predictions[i] == 1:
-            tp += 1
-        elif labels[i] == 1 and predictions[i] == 0:
-            fn += 1
-        elif labels[i] == 0 and predictions[i] == 1:
-            fp += 1
-        else:
-            tn += 1
-
+    tp, fp, tn, fn = confusion_matrix(labels, predictions)
     accuracy = (tp + tn) / (tp + tn + fp + fn)
     precision = tp / (tp + fp)
     recall = tp / (tp + fn)
@@ -119,34 +112,94 @@ def evaluate(labels, predictions):
 def main():
     # Load data from spreadsheet and split into train and test sets
     features, labels = load_data("spambase.csv")
+
     features = preprocess(features)
+
     X_train, X_test, y_train, y_test = train_test_split(
         features, labels, test_size=TEST_SIZE
     )
 
-    # Train a k-NN model and make predictions
-    model_nn = NN(X_train, y_train)
-    predictions = model_nn.predict(X_test, K)
-    accuracy, precision, recall, f1 = evaluate(y_test, predictions)
+    print("Preprocessing data...")
 
-    # Print results
-    print("**** 1-Nearest Neighbor Results ****")
-    print("Accuracy: ", accuracy)
-    print("Precision: ", precision)
-    print("Recall: ", recall)
-    print("F1: ", f1)
+    # Train a k-NN model and make predictions
+    model_knn = NN(X_train, y_train)
+    knn_predictions = model_knn.predict(X_test, K)
+    knn_tp, knn_fp, knn_tn, knn_fn = confusion_matrix(y_test, knn_predictions)
+    knn_accuracy, knn_precision, knn_recall, knn_f1 = evaluate(y_test, knn_predictions)
 
     # Train an MLP model and make predictions
-    model = train_mlp_model(X_train, y_train)
-    predictions = model.predict(X_test)
-    accuracy, precision, recall, f1 = evaluate(y_test, predictions)
+    model_mlp = train_mlp_model(X_train, y_train)
+    mlp_predictions = model_mlp.predict(X_test)
+    mlp_tp, mlp_fp, mlp_tn, mlp_fn = confusion_matrix(y_test, mlp_predictions)
+    mlp_accuracy, mlp_precision, mlp_recall, mlp_f1 = evaluate(y_test, mlp_predictions)
+    print("Data preprocessed.")
 
-    # Print results
-    print("**** MLP Results ****")
-    print("Accuracy: ", accuracy)
-    print("Precision: ", precision)
-    print("Recall: ", recall)
-    print("F1: ", f1)
+    print("Plotting results...")
+    # progress bar
+    for i in tqdm(range(100)):
+        time.sleep(0.02)
+
+    # Plot confusion matrix
+    confusion_matrix_fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+    confusion_matrix_fig.suptitle("Confusion Matrix")
+    ax1.matshow([[mlp_tp, mlp_fp], [mlp_fn, mlp_tn]], cmap=plt.cm.Blues)
+    ax1.set_title("MLP")
+    ax1.set_xlabel("Predicted")
+    ax1.set_ylabel("Actual")
+    ax1.set_xticks([0, 1])
+    ax1.set_yticks([0, 1])
+    ax1.set_xticklabels(["Spam", "Not Spam"])
+    ax1.set_yticklabels(["Spam", "Not Spam"])
+    ax1.text(0, 0, mlp_tp, ha="center", va="center", color="black")
+    ax1.text(0, 1, mlp_fn, ha="center", va="center", color="black")
+    ax1.text(1, 0, mlp_fp, ha="center", va="center", color="black")
+    ax1.text(1, 1, mlp_tn, ha="center", va="center", color="black")
+    ax2.matshow([[knn_tp, knn_fp], [knn_fn, knn_tn]], cmap=plt.cm.Blues)
+    ax2.set_title("k-NN")
+    ax2.set_xlabel("Predicted")
+    ax2.set_ylabel("Actual")
+    ax2.set_xticks([0, 1])
+    ax2.set_yticks([0, 1])
+    ax2.set_xticklabels(["Spam", "Not Spam"])
+    ax2.set_yticklabels(["Spam", "Not Spam"])
+    ax2.text(0, 0, knn_tp, ha="center", va="center", color="black")
+    ax2.text(0, 1, knn_fn, ha="center", va="center", color="black")
+    ax2.text(1, 0, knn_fp, ha="center", va="center", color="black")
+    ax2.text(1, 1, knn_tn, ha="center", va="center", color="black")
+
+    results_fig, ax3 = plt.subplots(1, 1, figsize=(15, 5))
+
+    # Plot results
+    results_fig.suptitle("Results")
+    ax3.bar(
+        np.arange(4),
+        [mlp_accuracy, mlp_precision, mlp_recall, mlp_f1],
+        width=0.3,
+        label="MLP",
+    )
+    ax3.bar(
+        np.arange(4) + 0.3,
+        [knn_accuracy, knn_precision, knn_recall, knn_f1],
+        width=0.3,
+        label="k-NN",
+    )
+    ax3.set_ylabel("Score")
+    ax3.set_xticks(np.arange(4) + 0.3 / 2)
+    ax3.set_xticklabels(["Accuracy", "Precision", "Recall", "F1"])
+    ax3.set_ylim([0, 1])
+    ax3.set_title("MLP vs k-NN")
+    ax3.text(0, mlp_accuracy, round(mlp_accuracy, 2), ha="center", va="bottom")
+    ax3.text(1, mlp_precision, round(mlp_precision, 2), ha="center", va="bottom")
+    ax3.text(2, mlp_recall, round(mlp_recall, 2), ha="center", va="bottom")
+    ax3.text(3, mlp_f1, round(mlp_f1, 2), ha="center", va="bottom")
+    ax3.text(0.3, knn_accuracy, round(knn_accuracy, 2), ha="center", va="bottom")
+    ax3.text(1.3, knn_precision, round(knn_precision, 2), ha="center", va="bottom")
+    ax3.text(2.3, knn_recall, round(knn_recall, 2), ha="center", va="bottom")
+    ax3.text(3.3, knn_f1, round(knn_f1, 2), ha="center", va="bottom")
+    ax3.legend(loc="lower right")
+
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == "__main__":
